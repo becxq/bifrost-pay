7260b8976c9b89d2e5a88706fbbd321ef692bf67
becxq <s74042176@gmail.com>
2026-05-25 13:51:52 +0500

    basic server configuration

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/becxq/bifrost-pay/api"
+
+	"google.golang.org/grpc"
+)
+
+// Server — наша структура, которая будет обрабатывать gRPC запросы
+type Server struct {
+	api.UnimplementedIdempotencyServiceServer
+}
+
+// CheckKey — реализация нашего gRPC метода
+func (s *Server) CheckKey(ctx context.Context, req *api.CheckKeyRequest) (*api.CheckKeyResponse, error) {
+	log.Printf("Получен запрос для ключа: %s", req.GetKey())
+
+	response := api.CheckKeyResponse{Status: "pending", Body: "", Code: 0}
+
+	return &response, nil
+}
+
+func main() {
+	// 1. Открываем TCP-порт для прослушивания
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	// 2. Создаем новый gRPC сервер
+	grpcServer := grpc.NewServer()
+
+	// 3. Регистрируем нашу структуру на этом gRPC сервере
+	api.RegisterIdempotencyServiceServer(grpcServer, &Server{})
+
+	log.Println("gRPC server is running on port :50051...")
+
+	// 4. Запускаем сервер
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
